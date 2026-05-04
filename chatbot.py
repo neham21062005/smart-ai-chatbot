57693ab23add1fafde59c78c5db91538efc5db0c
neham21062005 <nehalakshmi5345@gmail.com>
2026-05-04 19:33:31 +0530

    Refactor get_response function to simplify logic
    Removed greeting and farewell responses from get_response function and adjusted similarity score threshold.

--- chatbot.py
+++ chatbot.py
@@ -18,24 +18,13 @@ vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(patterns)
 
 def get_response(user_input):
-    user_input = user_input.lower()
-
-    
-    if any(word in user_input for word in ["hi", "hello", "hey"]):
-        return "Hello!"
-
-    if any(word in user_input for word in ["project", "ai"]):
-        return "Try Resume Analyzer, Chatbot, or Prediction models."
-
-    if any(word in user_input for word in ["bye"]):
-        return "Goodbye!"
-
-    
     user_vec = vectorizer.transform([user_input])
     similarity = cosine_similarity(user_vec, X)
-    score = similarity.max()
 
-    if score < 0.7:
+    score = similarity.max()
+    print("DEBUG SCORE:", score)   
+
+    if score < 0.6:
         return "Sorry, I didn't understand that."
 
     index = similarity.argmax()
